387306f875d864df745167dc62151517153c8e6f
paul-mcglinchey <paulmcglinchey789@gmail.com>
2023-02-01 21:28:01 +0000

    Made nav links work

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
 import './assets/main.css'
-import { RouterView, useRoute } from 'vue-router'
+import { RouterView } from 'vue-router'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 import NavLink from './components/NavLink.vue'
-
-const route = useRoute()
-
 </script>
 
 <template>
-  <div class="mx-5">
+  <div class="mx-5 mt-2">
     <header>
       <div class="flex justify-between">
         <Header msg="Bio-tracker" />
--- src/components/NavLink.vue
+++ src/components/NavLink.vue
@@ -5,17 +5,32 @@ import { RouterLink, useLink, useRoute } from 'vue-router';
 defineProps<{ to: string }>()
 
 const route = useRoute()
+const matched = ref(route.matched)
+
+watch(route, () => {
+  matched.value = route.matched
+
+  console.log(matched.value.some(({ name }) => name === "about"));
+})
 
 </script>
 
 <template>
-  <RouterLink :active="route.matched.some(({ name }) => name === to)" :to="to" class="font-bold active:text-lg text-sm group lowercase font-mono">
+  <RouterLink 
+    :to="to"
+    v-slot="{ isActive }"
+    active-class=""
+    :class="['font-bold text-lg group lowercase font-mono']">
     <span
       :class="[
         'relative inline-block px-2',
-        'before:block before:absolute before:-inset-1 before:-skew-y-2 before:h-5/6 before:top-1 before:bg-gray-900 before:scale-x-0 before:origin-left before:group-hover:scale-x-100 before:transition-all before:ease-in-out before:duration-200'
+        'before:block before:absolute before:-inset-1 before:-skew-y-2 before:h-5/6 before:top-1 before:bg-gray-900 before:scale-x-0 before:origin-left before:group-hover:scale-x-100 before:transition-all before:ease-in-out before:duration-200',
+        isActive && 'before:scale-x-100'
       ]">
-      <span class="relative text-indigo-500 group-hover:text-white transition-colors ease-in-out">
+      <span :class="[
+        'relative text-indigo-500 group-hover:text-white transition-colors ease-in-out',
+        isActive && 'text-white'
+      ]">
         <slot />
       </span>
     </span>
