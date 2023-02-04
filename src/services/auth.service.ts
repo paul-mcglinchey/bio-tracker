1f1c2d1ab7d9ad832423c239b883d919da8b9aae
paul-mcglinchey <paulmcglinchey789@gmail.com>
2023-02-04 00:13:10 +0000

    cleaning up auth service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -17,17 +17,11 @@ export class AuthService {
     this.pb = pb
   }
 
-  logout = () => {
-    this.pb.authStore.clear()
-  }
+  logout = () => this.pb.authStore.clear()
 
-  login = async (usernameOrEmail: string, password: string) => {
-    await this.pb.collection('users').authWithPassword(usernameOrEmail, password)
-  }
+  login = async (usernameOrEmail: string, password: string) => await this.pb.collection('users').authWithPassword(usernameOrEmail, password)
 
-  signup = async (values: ISignupFormValues) =>
-    await this.pb.collection('users').create(values)
+  signup = async (values: ISignupFormValues) => await this.pb.collection('users').create(values)
 
-  requestVerification = async (email: string) =>
-    await this.pb.collection('users').requestVerification(email)
+  requestVerification = async (email: string) => await this.pb.collection('users').requestVerification(email)
 }
