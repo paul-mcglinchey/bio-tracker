2ce5b826b0fb29bd3aa1b3ed762e90ea960c7f10
paul-mcglinchey <paulmcglinchey789@gmail.com>
2023-02-04 03:10:33 +0000

    Adjusted base directory

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bio-tracker/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
