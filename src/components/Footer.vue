9dd303282bbf49a664b1e6853bd350344595fa2d
paul-mcglinchey <paulmcglinchey789@gmail.com>
2023-02-04 17:19:15 +0000

    Added version tag to FE Incremented app version to 1.0.1

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+var version = APP_VERSION
 </script>
 
 <template>
@@ -6,10 +7,11 @@
     href="https://github.com/paul-mcglinchey/bio-tracker" 
     class="fixed left-0 bottom-0 text-center w-screen text-sm font-bold lowercase pb-2 group"
   >
+    <span class="border-r-2 mr-2 pr-2">v{{ version }}</span>
     <span class="inline-block group-hover:-translate-x-full group-hover:scale-x-0 transition-transform origin-right">
       Developed by
     </span>
-    <span class="inline-block text-indigo-800 group-hover:-translate-x-1/2 group-hover:scale-150 origin-bottom group-hover:bg-gray-900 group-hover:text-indigo-500 rounded px-1 py-0.5 transition-transform ease-in-out duration-300">
+    <span class="inline-block text-indigo-800 group-hover:-translate-x-2/3 origin-bottom group-hover:bg-gray-900 group-hover:text-indigo-500 rounded px-1 py-0.5 transition-transform ease-in-out duration-300">
       <span class="hidden group-hover:inline">@</span>
       <span>
         Paul<span class="group-hover:hidden before:content-['_']"></span>McGlinchey
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+process.env.VUE_APP_VERSION = process.env.npm_package_version
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/bio-tracker/',
@@ -12,5 +14,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  define: {
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+  },
 })
