b8a10f29de29e7e7d6c44c4955f3fc1c1be94a83
paul-mcglinchey <paulmcglinchey789@gmail.com>
2023-02-15 21:14:12 +0000

    Added privacy policy

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,19 +3,24 @@ var version = APP_VERSION
 </script>
 
 <template>
-  <a
-    href="https://github.com/paul-mcglinchey/bio-tracker" 
-    class="bg-neutral-100 lg:bg-transparent fixed left-0 bottom-0 text-center w-screen text-sm font-bold lowercase lg:pb-2 group"
-  >
+  <div class="fixed left-0 bottom-0 text-center w-screen text-sm lowercase">
+    <a class="hidden md:inline-block border-r-2 mr-2 pr-2 text-indigo-900 hover:text-indigo-500" href="privacypolicy.html">
+      <span>Privacy Policy</span>
+    </a>
     <span class="border-r-2 mr-2 pr-2">v{{ version }}</span>
-    <span class="inline-block group-hover:-translate-x-full group-hover:scale-x-0 transition-transform origin-right">
-      Developed by
-    </span>
-    <span class="inline-block text-indigo-800 group-hover:-translate-x-2/3 origin-bottom group-hover:bg-gray-900 group-hover:text-indigo-500 rounded px-1 py-0.5 transition-transform ease-in-out duration-300">
-      <span class="hidden group-hover:inline">@</span>
-      <span>
-        Paul<span class="group-hover:hidden before:content-['_']"></span>McGlinchey
+    <a
+      href="https://github.com/paul-mcglinchey/bio-tracker" 
+      class="bg-neutral-100 lg:bg-transparent lowercase lg:pb-2 group"
+    >
+      <span class="inline-block group-hover:-translate-x-full group-hover:scale-x-0 transition-transform origin-right">
+        Created by
       </span>
-    </span>
-  </a>
+      <span class="inline-block text-indigo-800 group-hover:-translate-x-2/3 origin-bottom group-hover:bg-gray-900 group-hover:text-indigo-500 rounded px-1 py-0.5 transition-transform ease-in-out duration-300">
+        <span class="hidden group-hover:inline">@</span>
+        <span>
+          Paul<span class="group-hover:hidden before:content-['_']"></span>McGlinchey
+        </span>
+      </span>
+    </a>
+  </div>
 </template>
